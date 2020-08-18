16944ef1d461ae94a0ea9169988a6cd2d1b6c30d
A.Hakim <bettayeb.a.hak@gmail.com>
2020-08-18 18:06:09 +0200

    Cart added to every new user

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from products.models import Product
+
+User = get_user_model()
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, related_name='cart', on_delete=models.CASCADE) # Deleting the cart while deleting the user 
+    items = models.ManyToManyField(Product)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return str(self.user)
+
+
+@receiver(post_save, sender=User)
+def create_user_cart(sender, instance, created, **kwargs):
+    if created:
+        Cart.objects.create(user=instance)
+    
